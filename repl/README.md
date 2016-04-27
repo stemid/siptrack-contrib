071c37da71318fa09c9c437799e6d0011c382a04
Stefan Midjich <swehack@gmail.com>
2016-04-27 11:43:54 +0200

    added a repl utility.

---
+++ repl/README.md
@@ -0,0 +1,7 @@
+# Siptrack REPL
+
+A helper utility to start a Python REPL with some Siptrack objects available to use the API live.
+
+# Usage
+
+    python -i siptrack_repl.py -c ../siptrack_live.cfg
---
+++ repl/siptrack_repl.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+from argparse import ArgumentParser
+from ConfigParser import RawConfigParser
+
+import siptracklib
+import siptracklib.errors
+
+parser = ArgumentParser()
+config = RawConfigParser()
+
+def st_connect():
+    args = parser.parse_args()
+    config.readfp(args.configuration)
+
+    st = siptracklib.connect(
+        config.get('siptrack', 'hostname'),
+        config.get('siptrack', 'username'),
+        config.get('siptrack', 'password'),
+        config.get('siptrack', 'port'),
+        use_ssl=config.getboolean('siptrack', 'ssl')
+    )
+
+    return(st)
+
+# TODO: Under construction 🏗 
+def st_refresh():
+    siptracklib.cm.disconnect()
+    return(st_connect())
+
+
+parser.add_argument(
+    '-v', '--verbose',
+    action='count',
+    default=False,
+    dest='verbose',
+    help='Verbose output, use more v\'s to increase level'
+)
+
+parser.add_argument(
+    '-c', '--configuration',
+    type=file,
+    required=True,
+    help='Configuration with siptrack connection credentials'
+)
+
+st = st_connect()
+
+st_view = st.view_tree.getChildByName(
+    config.get('import', 'base_view'),
+    include=['view']
+)
+
+st_local_users = st.view_tree.user_manager.listChildren(include=['user local'])
+st_passtree = st_view.listChildren(include=['password tree'])[0]
+session_user = st.getSessionUser()
+device_tree = st_view.listChildren(include=['device tree'])[0]
+user_manager = st.view_tree.user_manager
+
+print(
+    '''Available pre-defined variables:
+    st = {st}
+    st_view = {st_view}
+    device_tree = {device_tree}
+    st_local_users = {st_local_users}
+    st_passtree = {st_passtree}
+    session_user = {session_user}
+    user_manager = {user_manager}
+    '''.format(
+        st=st,
+        st_view=st_view,
+        st_local_users=st_local_users,
+        st_passtree=st_passtree,
+        session_user=session_user,
+        device_tree=device_tree,
+        user_manager=user_manager
+    )
+)
